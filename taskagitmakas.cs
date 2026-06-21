385d84d0b628b4c923a8adeb3c445e1a93306627
Miran <miranaktun@gmail.com>
2026-06-21 16:46:13 +0300

    Kurallar yazıldı

--- taskagitmakas.cs
+++ taskagitmakas.cs
@@ -2,3 +2,16 @@
 public Class taskagıtmakas{
     
 }
+
+{
+print("     TAŞ-KAĞIT-MAKAS OYUN KURALLARI")
+print("1. Taş, makası yener.")
+print("2. Makas, kağıdı yener.")
+print("3. Kağıt, taşı yener.")
+print("4. Aynı seçimlerde oyun berabere biter.")
+
+print(" TAŞ-KAĞIT-MAKAS OYUNUNA HOŞ GELDİNİZ! ")
+    print("Kurallar: Taş makası kırar, Makas kağıdı keser, Kağıt taşı sarar.")
+    print("Oyundan çıkmak için 'çıkış' yazabilirsiniz.")
+    print("3 puana ulaşan oyunu kazanır!")
+}
