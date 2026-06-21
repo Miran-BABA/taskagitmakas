aa33d33a94c9d1910c4d2f7796e55423133ff751
comet <132081997+cometwarrior0@users.noreply.github.com>
2026-06-21 16:44:23 +0300

    secenekler string dizisi ve rastgele tas kagit makas secimi eklendi

--- taskagitmakas.cs
+++ taskagitmakas.cs
@@ -1,4 +1,14 @@
 
-public Class taskagıtmakas{
-    
+// rastgele degisken
+// 1 tanesi atansin
+
+public class TasKagitMakas
+{
+    public string[] secenekler = ["tas", "kagit", "makas"];
+
+    private static Random rnd = new Random();
+    public string RastgeleSecim()
+    {
+        return secenekler[rnd.Next(0, 3)];
+    }
 }
