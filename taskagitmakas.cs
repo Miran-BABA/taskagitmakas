5ee9e1f3c298f20870833b26a4844d7f9fb378b1
Enes.Sarp.Sanin <enessarpsanin@gmail.com>
2026-06-21 16:48:04 +0300

    program sonlandırma kodu eklendi

--- taskagitmakas.cs
+++ taskagitmakas.cs
@@ -1,4 +1,12 @@
 
-public Class taskagıtmakas{
-    
+public class taskagıtmakas{
+    public void exitProgram()
+    {
+        string button = Console.ReadLine();
+        if (button == "k")
+        {
+            Console.WriteLine("Taş kağıt makas programı sonlandırıldı.");
+            Environment.Exit(0);
+        }
+    }
 }
